7ab1092639e6f078523dd10f3bb438a5a391abeb
gladys <achandogladys6@gmail.com>
2025-06-22 17:35:12 +0300

    add new changes to index.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,9 @@ function handlePostClick(postId){
     const detail=document.getElementById('post-detail');
     detail.innerHTML= `
         <h2>${post.title}</h2>
+        <img src="${post.image}" alt="${post.title}"/>
         <p><strong>Author:</strong> ${post.author}</p>
+        <p><strong>Date:</strong>${post.date}</p>
         <p>${post.content}</p>
       `;
   })
@@ -36,3 +38,33 @@ function handlePostClick(postId){
 }
 
 
+function addNewPost(){
+  const form=document.getElementById('post-form');
+  form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    const title=document.getElementById('newTitle').Value;
+    const image=document.getElementById('newImage').value;
+    const author=document.getElementById('newAuthor').value;
+    const content=document.getElementById('newContent').value;
+    const date=document.getElementById('newDate').value;
+
+    const newPost={
+      title,
+      image,
+      author,
+      content,
+      date
+    };
+
+    fetch('http://localhost:3000/posts',{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+        Accept:"application/json"
+      },
+      body:JSON.stringify(newPost)
+    })
+
+  })
+}
